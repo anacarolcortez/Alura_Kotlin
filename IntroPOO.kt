82c500201630810ef32329aee47bc7102031888b
Ana Carol Cortez <56210395+anacarolcortez@users.noreply.github.com>
2020-09-19 15:25:56 -0300

    Update IntroPOO.kt

--- IntroPOO.kt
+++ IntroPOO.kt
@@ -1,7 +1,7 @@
 fun main(){
     println("Bem-vinde ao Bytebank!")
 
-    for (i in 3 downTo 1){ // i in 3 downTo 1 para regressiva
+    for (i in 3 downTo 1){ // i in 1..3 para crescente
         println(i)
     }
     println("Acesso liberado")
@@ -9,12 +9,12 @@ fun main(){
     val contaCarol = Conta()
     contaCarol.titular = "Carol Cortez"
     contaCarol.numero = 123456
-    contaCarol.saldo = 100.015
+    contaCarol.setSaldo(100.015)
 
     val contaAna = Conta()
     contaAna.titular = "Ana Cortez"
     contaAna.numero = 123487
-    contaAna.saldo = 50.0
+    contaAna.setSaldo(50.0)
 
     contaCarol.depositar(500.0)
     contaAna.sacar(50.0)
@@ -25,7 +25,7 @@ fun main(){
 class Conta {
     var titular = ""
     var numero = 0
-    var saldo = 0.0
+    private var saldo = 0.0
 
 
     fun depositar(valor: Double) {
@@ -62,6 +62,17 @@ class Conta {
             saldo == 0.0 -> println("Saldo zerado")
             else -> println("Saldo negativo em R$ ${saldo}. Gostaria de contratar um empréstimo?")
         }
+    }
+
+    fun getSaldo(): Double { //encapsulamento
+        return saldo
+    }
+
+    fun setSaldo(valor:Double){
+        if (valor > 0) {
+            saldo = valor
+        }
+        
     }
 
 }
