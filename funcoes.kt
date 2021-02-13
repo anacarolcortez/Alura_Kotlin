b3bc5c617c53f41b23a0b144e3871f0d6930e1f8
Ana Carol Cortez <carolina.cortez@aluno.ifsp.edu.br>
2021-02-13 13:28:04 -0300

    funcoes

--- funcoes.kt
+++ funcoes.kt
@@ -10,8 +10,15 @@ fun main() {
     println("Resultado da soma: $sumNumbers")
 }
 
+//ordem superior
 fun sum(a:Int, b:Int) = a.plus(b)
 
 fun calculate(a:Int, b:Int, operation:(Int, Int) -> Int): Int{
     return operation(a,b)
 }
+
+//single line
+private fun getFullName(name:String, lastName:String) = "$name $lastName"
+
+//extensões: cria uma função que só pode ser chamada por um tipo específico, cujo valor pode ser referenciado dentro da função por meio da palavra this
+fun String.randomCapitalizedLetter() = this[(0..this.length-1).random()].toUpperCase()
